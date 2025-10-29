ee6573fc9e945d4e59995f7c846992f4c77eafc0
vera12704 <lverunchik27@gmail.com>
2025-10-29 16:50:41 +0300

    2 лаба

--- lukovaverakt-42-22/Controllers/WeatherForecastController.cs
+++ lukovaverakt-42-22/Controllers/WeatherForecastController.cs
@@ -2,32 +2,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace lukovaverakt_42_22.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
+        [ApiController]
+        [Route("[controller]")]
+        public class WeatherForecastController : ControllerBase
         {
+            private static readonly string[] Summaries = new[]
+            {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+            private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            public WeatherForecastController(ILogger<WeatherForecastController> logger)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                _logger = logger;
+            }
+
+            [HttpGet(Name = "GetWeatherForecast")]
+            public IEnumerable<WeatherForecast> Get()
+            {
+                _logger.LogError("Method was called");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            [HttpPost(Name = "AddNewSummary")]
+            public string[] AddNewSummary(string newSummary)
+            {
+                _logger.LogError("New method was called");
+                var list = Summaries.ToList();
+                list.Add(newSummary);
+                return list.ToArray();
+            }
         }
     }
-}
